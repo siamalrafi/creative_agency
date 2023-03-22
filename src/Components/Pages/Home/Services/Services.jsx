9c4b87ad67435f8694a10f51c66a5c4aa2ce19cd
siamalrafi <rahathasandipto@gmail.com>
2023-03-22 11:42:58 +0600

    This is the next part

--- src/Components/Pages/Home/Services/Services.jsx
+++ src/Components/Pages/Home/Services/Services.jsx
@@ -1,22 +1,9 @@
 import { Grid } from '@mui/material';
 import React from 'react';
-import { styled } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
 import ServieTitle from "./serviceTitlte";
-import SingleService from "./SingleService";
+import Service from './SingleService'; 
 
-
-
-
-
-
-const Services = () => {
-
-
-
-
-
+const Services = () => { 
     const serviceImages = [
         'https://i.ibb.co/HNhykGp/iphone-1.png',
         'https://i.ibb.co/r0gHkvM/color-palette-1.png',
@@ -25,7 +12,6 @@ const Services = () => {
         'https://i.ibb.co/r0gHkvM/color-palette-1.png',
         'https://i.ibb.co/CvQwzKC/coding-1.png',
     ];
-
 
     return (
         <div>
@@ -36,7 +22,7 @@ const Services = () => {
             <Grid container spacing={2}>
                 {serviceImages.map((image, index) => (
                     <Grid item xs={12} sm={6} md={4} key={index}>
-                        <SingleService image={image} key={index} />
+                        <Service image={image} key={index} />
                     </Grid>
                 ))}
             </Grid>
--- src/Components/Pages/Home/Services/SingleService.jsx
+++ src/Components/Pages/Home/Services/SingleService.jsx
@@ -1,110 +1,54 @@
-import * as React from 'react';
-import { styled } from '@mui/material/styles';
+import React from 'react';
 import Card from '@mui/material/Card';
-import CardHeader from '@mui/material/CardHeader';
-import CardMedia from '@mui/material/CardMedia';
 import CardContent from '@mui/material/CardContent';
-import CardActions from '@mui/material/CardActions';
-import Collapse from '@mui/material/Collapse';
-import Avatar from '@mui/material/Avatar';
-import IconButton from '@mui/material/IconButton';
+import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
-import { red } from '@mui/material/colors';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import ShareIcon from '@mui/icons-material/Share';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
 
-const ExpandMore = styled((props) => {
-    const { expand, ...other } = props;
-    return <IconButton {...other} />;
-})(({ theme, expand }) => ({
-    transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
-    marginLeft: 'auto',
-    transition: theme.transitions.create('transform', {
-        duration: theme.transitions.duration.shortest,
-    }),
-}));
+const Service = ({ image }) => {
+   return (
+      <Card
+         sx={{
+            maxWidth: 345,
+            boxShadow: 'none',
+            pt: 2,
+            mx: 'auto',
+            '&:hover': {
+               boxShadow: '0 0 10px 0 rgba(0, 0, 0, 0.1)',
+            },
+         }}
+      >
+         <CardMedia
+            component='img'
+            height='140'
+            image={image}
+            alt='green iguana'
+            sx={{
+               width: 'auto',
+               mx: 'auto',
+            }}
+         />
+         <CardContent
+            sx={{
+               textAlign: 'center',
+            }}
+         >
+            <Typography
+               gutterBottom
+               variant='h5'
+               component='div'
+               sx={{
+                  fontWeight: 'bold',
+               }}
+            >
+               Web & Mobile design
+            </Typography>
+            <Typography variant='body2' color='text.secondary'>
+               We craft stunning and amazing web UI, using a well drrafted UX to
+               fit your product.
+            </Typography>
+         </CardContent>
+      </Card>
+   );
+};
 
-export default function SingleService({ image }) {
-    const [expanded, setExpanded] = React.useState(false);
-
-    const handleExpandClick = () => {
-        setExpanded(!expanded);
-    };
-
-    return (
-        <Card sx={{ mr: "2px", maxWidth: 345, alignItems: "center" }}>
-            <CardHeader
-                sx={{ textAlign: "center" }}
-                avatar={
-                    <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-                        <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" alt="avatar" />
-                    </Avatar>
-                }
-                action={
-                    <IconButton aria-label="settings">
-                        <MoreVertIcon />
-                    </IconButton>
-                }
-                title="Shrimp and Chorizo Paella"
-                subheader="September 14, 2016"
-            />
-            <CardMedia
-                component="img"
-                height="194"
-                image={image}
-                alt="Paella dish"
-            />
-            <CardContent>
-                <Typography variant="body2" color="text.secondary">
-                    This impressive paella is a perfect party dish and a fun meal to cook
-                    together with your guests. Add 1 cup of frozen peas along with the mussels,
-                    if you like.
-                </Typography>
-            </CardContent>
-            <CardActions disableSpacing>
-                <IconButton aria-label="add to favorites">
-                    <FavoriteIcon />
-                </IconButton>
-                <IconButton aria-label="share">
-                    <ShareIcon />
-                </IconButton>
-                <ExpandMore
-                    expand={expanded}
-                    onClick={handleExpandClick}
-                    aria-expanded={expanded}
-                    aria-label="show more"
-                >
-                    <ExpandMoreIcon />
-                </ExpandMore>
-            </CardActions>
-            <Collapse in={expanded} timeout="auto" unmountOnExit>
-                <CardContent>
-                    <Typography paragraph>Method:</Typography>
-                    <Typography paragraph>
-                        Heat 1/2 cup of the broth in a pot until simmering, add saffron and set
-                        aside for 10 minutes.
-                    </Typography>
-                    <Typography paragraph>
-                        Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over
-                        medium-high heat. Add chicken, shrimp and chorizo, and cook, stirring
-                        occasiona ickened and fragrant, about 10 minutes. Add
-                        saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
-                    </Typography>
-                    <Typography paragraph>
-                        Add rice and stir very gently to distribute. Top with artichokes and
-                        peppers, and cook without stirring, until most of the liquid is absorbed,
-                        15 to 18 minutes. Reduce heat to medium-low, add reserved shrimp and
-                        mussels, tucking them down into the rice, and cook again without
-                        stirring, until mussels have opened and rice is just tender, 5 to 7
-                        minutes more. (Discard any mussels that don&apos;t open.)
-                    </Typography>
-                    <Typography>
-                        Set aside off of the heat to let rest for 10 minutes, and then serve.
-                    </Typography>
-                </CardContent>
-            </Collapse>
-        </Card>
-    );
-}
+export default Service;
