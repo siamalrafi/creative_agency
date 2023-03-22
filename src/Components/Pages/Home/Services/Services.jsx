192b17b781d9c23da4e5c568241aa3e1ce50d962
siamalrafi <rahathasandipto@gmail.com>
2023-03-22 11:34:32 +0600

    This is hte  textAlign: center

--- src/Components/Pages/Home/Services/Services.jsx
+++ src/Components/Pages/Home/Services/Services.jsx
@@ -1,14 +1,45 @@
+import { Grid } from '@mui/material';
 import React from 'react';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
 import ServieTitle from "./serviceTitlte";
+import SingleService from "./SingleService";
+
+
+
 
 
 
 const Services = () => {
+
+
+
+
+
+    const serviceImages = [
+        'https://i.ibb.co/HNhykGp/iphone-1.png',
+        'https://i.ibb.co/r0gHkvM/color-palette-1.png',
+        'https://i.ibb.co/CvQwzKC/coding-1.png',
+        'https://i.ibb.co/HNhykGp/iphone-1.png',
+        'https://i.ibb.co/r0gHkvM/color-palette-1.png',
+        'https://i.ibb.co/CvQwzKC/coding-1.png',
+    ];
+
+
     return (
         <div>
             <ServieTitle />
+            {/* ----------- SingleService ----------- */}
+            {/* <SingleService /> */}
 
-
+            <Grid container spacing={2}>
+                {serviceImages.map((image, index) => (
+                    <Grid item xs={12} sm={6} md={4} key={index}>
+                        <SingleService image={image} key={index} />
+                    </Grid>
+                ))}
+            </Grid>
 
         </div>
     );
