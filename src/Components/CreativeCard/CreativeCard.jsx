a9ef37a730beb07689638e28e6387cea71d0f5ee
siamalrafi <rahathasandipto@gmail.com>
2023-03-22 13:23:41 +0600

    next

--- src/Components/CreativeCard/CreativeCard.jsx
+++ src/Components/CreativeCard/CreativeCard.jsx
@@ -1,31 +1,80 @@
-import * as React from 'react';
+import React from 'react';
 import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-import { Button, CardActionArea, CardActions } from '@mui/material';
+import Box from '@mui/material/Box';
+import IconButton from '@mui/material/IconButton';
 
-export default function MultiActionAreaCard() {
-    return (
-        <Card sx={{ maxWidth: 345 }}>
-            <CardActionArea>
-                <CardMedia
-                    component="img"
-                    height="140"
-                    image="https://images.unsplash.com/photo-1593789198788-8b21805d5fdb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
-                    alt="green iguana"
-                />
-                <CardContent>
-                    <Typography gutterBottom variant="h5" component="div">
-                        Lizard
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary">
-                        Lizards are a widespread group of squamate reptiles, with over 6,000
-                        species, ranging across all continents except Antarctica
-                    </Typography>
-                </CardContent>
-            </CardActionArea>
+import AddIcon from '@mui/icons-material/Add';
 
-        </Card>
-    );
-}
+import { useTheme } from '@mui/material'
+
+
+
+
+const CreativeCard = ({image}) => {
+
+    const theme = useTheme()
+
+   return (
+      <Card sx={{ maxWidth: 345, p: 2, borderRadius: '10px', minWidth: '345px' }}>
+         <CardMedia
+            component='img'
+            height='190'
+            image={image}
+            alt='green iguana'
+            style={{
+               borderRadius: '10px',
+            
+            }}
+         />
+         <CardContent
+            sx={{
+               p: 0,
+               pb: 0,
+               pt: 4,
+               '&:last-child': {
+                  pb: 0,
+               },
+               display: 'flex',
+               justifyContent: 'space-between',
+               alignItems: 'center',
+            }}
+         >
+            <Box>
+               <Typography
+                  gutterBottom
+                  variant='body'
+                  component='div'
+                  color='text.secondary'
+               >
+                  Web design
+               </Typography>
+               <Typography
+                  variant='body2'
+                  color='primary.main'
+                  fontWeight={600}
+                  fontSize={20}
+               >
+                  Web design research
+               </Typography>
+            </Box>
+            <IconButton sx={{
+                border: `1px solid ${theme.palette.primary.main}`,
+                height: '30px',
+                width: '30px',
+                '& svg': {
+                    color: 'primary.main'
+                }
+            }}>
+               <AddIcon />
+            </IconButton>
+         </CardContent>
+      </Card>
+   );
+};
+
+export default CreativeCard;
--- src/Components/Pages/Home/OurWorks/InterestingCard.jsx
+++ src/Components/Pages/Home/OurWorks/InterestingCard.jsx
@@ -6,30 +6,30 @@ import Typography from '@mui/material/Typography';
 import { Button, CardActionArea, CardActions } from '@mui/material';
 
 export default function InterestingCard() {
-  return (
-    <Card sx={{ maxWidth: 345 }}>
-      <CardActionArea>
-        <CardMedia
-          component="img"
-          height="140"
-          image="/static/images/cards/contemplative-reptile.jpg"
-          alt="green iguana"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            Lizard
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            Lizards are a widespread group of squamate reptiles, with over 6,000
-            species, ranging across all continents except Antarctica
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
-        <Button size="small" color="primary">
-          Share
-        </Button>
-      </CardActions>
-    </Card>
-  );
+    return (
+        <Card sx={{ maxWidth: 345 }}>
+            <CardActionArea>
+                <CardMedia
+                    component="img"
+                    height="140"
+                    image="/static/images/cards/contemplative-reptile.jpg"
+                    alt="green iguana"
+                />
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                        Lizard
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                        Lizards are a widespread group of squamate reptiles, with over 6,000
+                        species, ranging across all continents except Antarctica
+                    </Typography>
+                </CardContent>
+            </CardActionArea>
+            <CardActions>
+                <Button size="small" color="primary">
+                    Share
+                </Button>
+            </CardActions>
+        </Card>
+    );
 }
